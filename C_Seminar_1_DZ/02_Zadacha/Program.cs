0c8f0537f24e034131cbe8f221fb3ff5888c68ad
a.fedotov <zacyla@gmail.com>
2022-07-15 18:37:24 +0300

    init

---
+++ C_Seminar_1_DZ/02_Zadacha/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт максимальное из этих чисел.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+Console.Write("Ведите 1ое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Ведите 2ое число: ");
+int num2 = int.Parse(Console.ReadLine());
+if (num1 > num2 ){
+    Console.WriteLine("Число 1 больше, чем число 2");
+}
+else Console.WriteLine("Число 2 больше, чем число 1");
---
+++ C_Seminar_1_DZ/04_Zadacha/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+Console.Write("Ведите 1ое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Ведите 2ое число: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.Write("Ведите 3eе число: ");
+int num3 = int.Parse(Console.ReadLine());
+int max = 0;
+
+if (num1 > num2)
+max = num1;
+else max = num2;
+
+if (max > num3)
+Console.WriteLine($"Максимальное число: {max} ");
+else 
+Console.WriteLine($"Максимальное число: {num3}");
+    
