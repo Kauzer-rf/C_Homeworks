3edc347ff6f4903108b69581157285ae64046942
a.fedotov <zacyla@gmail.com>
2022-08-04 18:33:55 +0300

    3rd is done

--- C_Seminar_4_DZ/Zadacha_29/Program.cs
+++ C_Seminar_4_DZ/Zadacha_29/Program.cs
@@ -6,4 +6,13 @@
 6, 1, 33 -> [6, 1, 33]
 
 */ 
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Для вывода массива, требуется ввести его элементы.");
+int[] ArrayElement = new int[8];
+
+for (int i = 0; i < 8; i++)
+{
+    Console.Write("Введите элемент c индексом " + i + ": ");
+    ArrayElement[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Получился массив: [{0}", String.Join(",", ArrayElement) + "]");
