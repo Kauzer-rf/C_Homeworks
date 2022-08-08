537be02baac637572ae01a2a47fb95f4ae634016
a.fedotov <zacyla@gmail.com>
2022-08-08 17:56:25 +0300

    36 is done

--- C_Seminar_5_DZ/Zadacha_36/Program.cs
+++ C_Seminar_5_DZ/Zadacha_36/Program.cs
@@ -5,3 +5,32 @@
 
 [-4, -6, 89, 6] -> 0
 */
+
+Console.Write("Введите длину массива: ");
+int len = int.Parse(Console.ReadLine());
+
+int[] arr = GetArray(len);
+
+int[] GetArray(int n)
+{
+    int[] arr = new int[n];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(-9, 10);
+    }
+    return arr;
+}
+
+Console.WriteLine("Сгенерированный массив: [{0}", String.Join(", ", arr) + "]");
+
+int res = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if (i % 2 != 0)
+    {
+        res += arr[i];
+    }
+
+}
+Console.WriteLine("Сумма элементов с нечетными индексами: " + res);
