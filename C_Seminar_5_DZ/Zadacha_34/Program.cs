c3683aa3c40974731e319842b6070220e6f94e11
a.fedotov <zacyla@gmail.com>
2022-08-08 17:51:44 +0300

    34 is done

--- C_Seminar_5_DZ/Zadacha_34/Program.cs
+++ C_Seminar_5_DZ/Zadacha_34/Program.cs
@@ -3,3 +3,31 @@
 
 [345, 897, 568, 234] -> 2
 */
+Console.Write("Введите длину массива: ");
+int len = int.Parse(Console.ReadLine());
+
+int[] arr = GetArray(len);
+
+int[] GetArray(int n)
+{
+    int[] arr = new int[n];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(100, 1000);
+    }
+    return arr;
+}
+
+Console.WriteLine("Сгенерированный массив: [{0}", String.Join(", ", arr) + "]");
+
+int res = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] % 2 == 0)
+    {
+        res++;
+    }
+
+}
+Console.WriteLine("Количество четных чисел в массиве: " + res);
