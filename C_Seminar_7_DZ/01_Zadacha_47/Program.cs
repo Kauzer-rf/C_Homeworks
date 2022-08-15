ab2e1b6685fec8b51c82fcad1fe8ad5f41fbdfc0
a.fedotov <zacyla@gmail.com>
2022-08-15 18:59:13 +0300

    47 is done

--- C_Seminar_7_DZ/01_Zadacha_47/Program.cs
+++ C_Seminar_7_DZ/01_Zadacha_47/Program.cs
@@ -9,3 +9,35 @@ m = 3, n = 4.
 
 8 7,8 -7,1 9
 */
+Console.Write("Введите количество срок и столбцов через пробел: ");
+string[] input = Console.ReadLine().Split();
+int row = int.Parse(input[0]);
+int col = int.Parse(input[1]);
+
+double[,] GetArray(int row, int col)
+{
+    double[,] array = new double[row, col];
+    Random rnd = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Math.Round(rnd.NextDouble() * 100 - 20, 2);
+        }
+    }
+    return array;
+}
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintArray(GetArray(row, col));
