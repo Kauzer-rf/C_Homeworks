6f2c6438523ca0890d1dbf4a29ccfab48bf97a44
a.fedotov <zacyla@gmail.com>
2022-08-21 21:30:59 +0300

    1st is ready

--- C_Seminar_8_DZ/01_Zadacha_54/Program.cs
+++ C_Seminar_8_DZ/01_Zadacha_54/Program.cs
@@ -1,13 +1,45 @@
 ﻿/*
 Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
-
-Например, задан массив:
-1 4 7 2
-5 9 2 3
-8 4 2 4
-
-В итоге получается вот такой массив:
-1 2 4 7
-2 3 5 9
-2 4 4 8
 */
+int[,] array = 
+{
+    {1, 4, 7, 2,},
+    {5, 9, 2, 3,},
+    {8, 4, 2, 4,}
+};
+void PrintArray(int[,]array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+void SortedArray(int[,]array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+        
+      }
+    }
+  }
+}
+Console.WriteLine("\nЗадан следующий массив: ");
+PrintArray(array);
+
+SortedArray(array);
+Console.WriteLine("\nУпорядоченный по убыванию (построчно) массив: ");
+PrintArray(array);
