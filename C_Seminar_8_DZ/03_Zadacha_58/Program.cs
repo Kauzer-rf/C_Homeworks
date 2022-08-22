1985997fd99746e87c50ede5b8e11d37764d914a
a.fedotov <zacyla@gmail.com>
2022-08-22 09:34:04 +0300

    3rd is ready

--- C_Seminar_8_DZ/03_Zadacha_58/Program.cs
+++ C_Seminar_8_DZ/03_Zadacha_58/Program.cs
@@ -22,3 +22,51 @@
 56 8 4 24
 10 6 24 49
 */
+int[,] array = new int[4, 4];
+int[,] array2 = new int[4, 4];
+
+CreateArray(array);
+CreateArray(array2);
+Console.WriteLine("\nСгенерирована первая матрица: ");
+PrintArray(array);
+Console.WriteLine("\nСгенерирована вторая матрица: ");
+PrintArray(array2);
+
+MultiArray(array, array2);
+Console.WriteLine("\nПроизведение матриц записано в массив: ");
+PrintArray(MultiArray(array, array2));
+
+void PrintArray(int[,]array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(1, 10);
+    }
+  }
+}
+int[,] MultiArray(int[,] array1, int[,] array2)
+{
+    int[,] multiplyArray = new int[array1.GetLength(0), array1.GetLength(1)];
+
+    for (int i = 0; i < array2.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2.GetLength(1); j++)
+        {
+            multiplyArray[i, j] = array1[i, j] * array2[i, j];
+        }
+    }
+    return multiplyArray;
+}
