f18cf78bbdcd25f535157b090c6ec18064a08341
a.fedotov <zacyla@gmail.com>
2022-08-22 09:59:22 +0300

    4ер is ready

--- C_Seminar_8_DZ/04_Zadacha_60/Program.cs
+++ C_Seminar_8_DZ/04_Zadacha_60/Program.cs
@@ -8,3 +8,41 @@
 
 45(1,0,0) 53(1,0,1)
 */
+int[,,] rnd3Darray = new int[3,3,3];
+GetArray(rnd3Darray);
+PrintArray(rnd3Darray);
+
+
+int[,,] GetArray(int[,,] array)
+{
+    int[,,] array2 = new int[array.GetLength(0), array.GetLength(1), array.GetLength(2)];
+    int item = 10;
+    for (int x = 0; x < array.GetLength(0); x++)
+    {
+        for (int y = 0; y < array.GetLength(1); y++)
+        {
+            for (int z = 0; z < array.GetLength(2); z++)
+            {
+                array[x, y, z] = item;
+                item++;
+            }
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,,] array)
+{
+    for (int k = 0; k < array.GetLength(2); k++)
+    {
+        Console.WriteLine($"\nПлоскость {k} : ");
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write($"[{i},{j},{k}] - {array[i, j, k]}" + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
