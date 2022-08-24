a4f21e7a77c4710a82864922b53be40d99e44a4c
a.fedotov <zacyla@gmail.com>
2022-08-24 14:01:09 +0300

    2nd is ready

--- C_Seminar_9_DZ/02_Zadacha_66/Program.cs
+++ C_Seminar_9_DZ/02_Zadacha_66/Program.cs
@@ -4,3 +4,18 @@
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30
 */
+Console.Write("\nВведите начальное число M: ");
+int M = int.Parse(Console.ReadLine());
+Console.Write("Введите конечное число N: ");
+int N = int.Parse(Console.ReadLine());
+
+int NaturalNum(int start, int end)
+{
+    if (start == end)
+    {
+        return start;
+    }
+    
+    return start + NaturalNum(++start, end);
+}
+Console.Write($"\nСумма чисел от {M} до {N} = {NaturalNum(M, N)}");
